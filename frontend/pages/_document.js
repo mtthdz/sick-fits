c0145ebcf7bfa4c3147244d75d61b2503f4aed30
mtthdz <matt@mtthdz.com>
2023-02-07 17:12:39 -0500

    09

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,19 +1,28 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
-
+import { ServerStyleSheet } from 'styled-components';
 
 // nextjs does not have a function api for hooks
 // next hides this, but we create this doc to add custom attributes
 // and css
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
-    return(
+    return (
       <Html lang="en-CA">
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-    </Html>
-    )
+      </Html>
+    );
   }
 }
