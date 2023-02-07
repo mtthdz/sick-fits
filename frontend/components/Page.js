a0a19f27dfb6959ab4cf1ed026d826f1b0a00efe
mtthdz <matt@mtthdz.com>
2023-02-06 19:22:00 -0500

    05

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,8 +1,10 @@
 import propTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <>
+      <Header />
       <h2>I am the page component</h2>
       {children}
     </>
