22d43634b513de899efcbb633e0e972932a72a84
mtthdz <matt@mtthdz.com>
2023-02-08 14:57:01 -0500

    14

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,11 +1,13 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
+import { User } from './schemas/User';
 
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // how long they should stay signed in
-  secret: process.env.COOKIE_SECRET
+  secret: process.env.COOKIE_SECRET,
 };
 
 export default config({
@@ -18,10 +20,10 @@ export default config({
   db: {
     adapter: 'mongoose',
     url: databaseURL,
-    // TODO: add data seeding here 
+    // TODO: add data seeding here
   },
   lists: createSchema({
-    // Schema items go here
+    User,
   }),
   ui: {
     // TODO: change this for roles
