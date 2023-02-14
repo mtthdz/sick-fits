87179c98eb683258363cd2e5e124df7f91a9b17e
mtthdz <matt@mtthdz.com>
2023-02-14 07:59:20 -0500

    24 - sell form styling

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,45 +1,68 @@
 import { useState } from 'react';
+import Form from './styles/Form';
 import useForm from '../lib/useForm';
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'nice shoes',
     price: 1234,
     description: 'nice',
   });
 
   return (
-    <form>
-      <label htmlFor="name">
-        name
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="name"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="price">
-        price
-        <input
-          type="number"
-          id="price"
-          name="price"
-          placeholder="price"
-          value={inputs.price}
-          onChange={handleChange}
-        />
-      </label>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label htmlFor="name">
+          name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-
-      <button type="button" onClick={resetForm}>
-        Reset Form
-      </button>
-    </form>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
