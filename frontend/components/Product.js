c67a07722f6a506ffc08d5c81ae360fea7e4e958
mtthdz <matt@mtthdz.com>
2023-02-17 08:16:21 -0500

    31 - delete product functionality

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -3,6 +3,7 @@ import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 
 export default function Product({ product }) {
   return (
@@ -16,7 +17,7 @@ export default function Product({ product }) {
       </Title>
       <PriceTag>{formatMoney(product.price)}</PriceTag>
       <p>{product.description}</p>
-      <div className="buttonlist">
+      <div className="buttonList">
         <Link
           href={{
             pathname: 'update',
@@ -27,6 +28,7 @@ export default function Product({ product }) {
         >
           Edit ✏️
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
     </ItemStyles>
   );
