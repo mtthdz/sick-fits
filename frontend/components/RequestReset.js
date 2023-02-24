61e157fdd375dc93845d5bb4d752a0440fd56717
mtthdz <matt@mtthdz.com>
2023-02-23 21:00:18 -0500

    41 - setup password reset on frontend

---
+++ frontend/components/RequestReset.js
@@ -0,0 +1,62 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import CURRENT_USER_QUERY from './User';
+import Error from './ErrorMessage';
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+  });
+
+  const [signup, { data, loading, error }] = useMutation(
+    REQUEST_RESET_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const res = await signup().catch(console.error);
+    console.log(res);
+    console.log({ data, loading, error });
+    resetForm();
+  }
+
+  return (
+    <Form method="post" onSubmit={handleSubmit}>
+      <h2>Request A Password Reset</h2>
+      <Error error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Success! Check your email for a link!</p>
+        )}
+
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">Request reset!</button>
+      </fieldset>
+    </Form>
+  );
+}
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
+import RequestReset from '../components/RequestReset';
 
 export default function SignInPage() {
   return (
     <GridStyles>
       <SignIn />
       <SignUp />
+      <RequestReset />
     </GridStyles>
   );
 }
