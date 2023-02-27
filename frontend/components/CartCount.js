0dc8e54a4dca8628285dbff127f10a072bdeab06
mtthdz <matt@mtthdz.com>
2023-02-27 18:55:30 -0500

    added animation to cart counter

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,3 +1,4 @@
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import styled from 'styled-components';
 
 const Dot = styled.div`
@@ -12,6 +13,44 @@ const Dot = styled.div`
   font-variant-numeric: tabular-nums;
 `;
 
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: transform 0.4s;
+    backface-visibility: hidden;
+  }
+  .count-enter {
+    transform: scale(4) rotate(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotate(0.5turn);
+  }
+`;
+
 export default function CartCount({ count }) {
-  return <Dot>{count}</Dot>;
+  return (
+    <AnimationStyles>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <Dot>{count}</Dot>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
+  );
 }
