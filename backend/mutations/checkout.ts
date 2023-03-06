c9c733d4b0ca26863372053e3afd1366722fef05
mtthdz <matt@mtthdz.com>
2023-03-05 23:19:27 -0500

    implemented db mutations on successful cart checkout

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -62,4 +62,31 @@ export default async function checkout(
     console.log(err);
     throw new Error(err.message);
   });
+
+  const orderItems = cartItems.map(cartItem => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    }
+
+    return orderItem;
+  })
+
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    }
+  });
+
+  // deletes all existing cart items
+  const cartItemIds = cartItems.map(cartItem => cartItem.id);
+  await context.lists.CartItem.deleteMany({ ids: cartItemIds });
+
+  return order;
 }
