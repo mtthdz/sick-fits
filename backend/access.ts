8b97c0b669c9e45834943096faffe838f2a0e47d
mtthdz <matt@mtthdz.com>
2023-03-07 08:17:28 -0500

    created permissions check on roles

--- backend/access.ts
+++ backend/access.ts
@@ -1,6 +1,23 @@
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 // checks if user is signed in via boolean
 export function isSignedIn({ session }: ListAccessArgs): boolean {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs): boolean {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('Matt');
+  },
+};
