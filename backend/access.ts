98e3277455468b1ebf5891124704cafb224a7fe5
mtthdz <matt@mtthdz.com>
2023-03-08 18:29:16 -0500

    added user crud permissions and rules

--- backend/access.ts
+++ backend/access.ts
@@ -1,6 +1,12 @@
 import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
+/**
+ * rules versus permissions:
+ * permissions: you must be checked under your role
+ * rules: either the checkbox or yourself
+ */
+
 // checks if user is signed in via boolean
 export function isSignedIn({ session }: ListAccessArgs): boolean {
   return !!session;
@@ -79,5 +85,19 @@ export const rules = {
     }
     // They should only see available products (based on the status field)
     return { status: 'AVAILABLE' };
+  },
+
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+
+    // check for permission
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+
+    // else, can only update themselves
+    return { id: session.itemId };
   },
 };
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,9 +1,18 @@
 import { text, password, relationship } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { permissions, rules } from '../access';
 
 export const User = list({
-  // access
-  // ui
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageUsers,
+    hideDelete: (args) => !permissions.canManageUsers,
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -17,7 +26,12 @@ export const User = list({
       },
     }),
     orders: relationship({ ref: 'Order.user', many: true }),
-    role: relationship({ ref: 'Role.assignedTo' }),
+    role: relationship({
+      ref: 'Role.assignedTo',
+      access: {
+        create: permissions.canManageUsers,
+      },
+    }),
     products: relationship({
       ref: 'Product.user',
       many: true,
